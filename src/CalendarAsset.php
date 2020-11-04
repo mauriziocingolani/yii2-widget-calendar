47b718ef5a60a0303852b49ab528f60cb4cc706e
Maurizio Cingolani <mauriziocingolani74@gmail.com>
2020-11-04 07:50:09 +0100

    Dipendenze assets

--- src/CalendarAsset.php
+++ src/CalendarAsset.php
@@ -9,6 +9,11 @@ class CalendarAsset extends AssetBundle {
     public function init() {
         $this->sourcePath = __DIR__ . '/assets';
         $this->css = ['css/mauriziocingolani-calendar.css'];
+        $this->depends = [
+            'yii\web\YiiAsset',
+            'app\assets\Bootstrap4',
+            'rmrevin\yii\fontawesome\AssetBundle',
+        ];
         parent::init();
     }
 
