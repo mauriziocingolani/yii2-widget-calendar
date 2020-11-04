57a6415fc3d7edf634b86f11d488263cf830925c
Maurizio Cingolani <mauriziocingolani74@gmail.com>
2020-11-04 11:30:19 +0100

    Predisposizione blocco modalità

--- src/Calendar.php
+++ src/Calendar.php
@@ -2,9 +2,21 @@
 
 namespace mauriziocingolani\calendar;
 
+use yii\base\InvalidConfigException;
 use yii\bootstrap\Widget;
 
 class Calendar extends Widget {
+
+    const MODE_MONTH = 'month';
+    const MODE_WEEK = 'week';
+    const MODE_DAY = 'day';
+
+    public $modes = [self::MODE_MONTH, self::MODE_WEEK, self::MODE_DAY];
+    public $mode;
+    public $year;
+    public $monthOrWeek;
+    public $day;
+    public $data;
 
     public function init() {
         CalendarAsset::register($this->view);
@@ -12,7 +24,20 @@ class Calendar extends Widget {
     }
 
     public function run(): string {
+        $this->_checkConfig();
         return $this->render('calendar');
+    }
+
+    public function getTitle() {
+        return "{titolo}";
+    }
+
+    private function _checkConfig() {
+        if (is_null($this->mode)) :
+            throw new InvalidConfigException(self::class . ': you must set the $mode attribute.');
+        elseif (!in_array($this->mode, $this->modes)) :
+            throw new InvalidConfigException(self::class . ': you should add the $mode you selected (' . $this->mode . ') in the $modes attribute array.');
+        endif;
     }
 
 }
--- src/views/calendar.php
+++ src/views/calendar.php
@@ -1,21 +1,58 @@
 <?php
-\yii\helpers\VarDumper::dump($this, 10, true);
+
+use mauriziocingolani\calendar\Calendar;
+use mauriziocingolani\yii2fmwkphp\Html;
+
+/* @var $this \mauriziocingolani\yii2fmwkphp\View */
+/* @var $calendar Calendar */
+$calendar = $this->context;
 ?>
 
 <div class="row mb-3">
+
+    <!-- MODE -->
     <div class="calendar-mode col-sm-4 text-center">
-        mode
+        <div class="btn-group" role="group" aria-label="...">
+            <?php if (array_search(Calendar::MODE_MONTH, $calendar->modes) !== false) : ?>
+                <a href="/" class="btn btn-default btn-xs <?= $calendar->mode == Calendar::MODE_MONTH ? 'btn-primary' : null; ?>">Mese</a>
+            <?php endif; ?>
+            <?php if (array_search(Calendar::MODE_WEEK, $calendar->modes) !== false) : ?>
+                <a href="/" class="btn btn-default btn-xs <?= $calendar->mode == Calendar::MODE_WEEK ? 'btn-primary' : null; ?>">Settimana</a>
+            <?php endif; ?>
+            <?php if (array_search(Calendar::MODE_DAY, $calendar->modes) !== false) : ?>
+                <a href="/" class="btn btn-default btn-xs <?= $calendar->mode == Calendar::MODE_DAY ? 'btn-primary' : null; ?>">Giorno</a>
+            <?php endif; ?>
+        </div>
     </div>
+
+    <!-- TITLE -->
     <div class="calendar-title col-sm-4 text-center">
-        title
+        <h4>{titolo...}</h4>
     </div>
+
+    <!-- CONTROLS -->
     <div class="calendar-controls col-sm-4 text-center">
-        controls
+        <div class="btn-group" role="group" aria-label="...">
+            <?=
+            Html::fasa('step-backward', null, ["/"], [
+                'class' => 'btn btn-xs btn-default',
+                'title' => '...',
+            ]) .
+            Html::a('Oggi', ["/"], [
+                'class' => 'btn btn-xs btn-default',
+            ]) .
+            Html::fasa('step-forward', null, ["/"], [
+                'class' => 'btn btn-xs btn-default',
+                'title' => '...',
+            ]);
+            ?>
+        </div>
     </div>
+
 </div>
 
 <div class="overflow-auto">
-    <table class="calendar month mb-3">
-        
+    <table class="calendar mb-3">
+
     </table>
 </div>
