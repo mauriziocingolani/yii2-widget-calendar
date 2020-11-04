52f81f0ae856fc89d5e2434f4dc3315d9db25b97
Maurizio Cingolani <mauriziocingolani74@gmail.com>
2020-11-04 10:21:43 +0100

    Predisposizione layout della view

--- src/views/calendar.php
+++ src/views/calendar.php
@@ -1,9 +1,21 @@
 <?php
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+\yii\helpers\VarDumper::dump($this, 10, true);
 ?>
 
-<h1>Calendario</h1>
+<div class="row mb-3">
+    <div class="calendar-mode col-sm-4 text-center">
+        mode
+    </div>
+    <div class="calendar-title col-sm-4 text-center">
+        title
+    </div>
+    <div class="calendar-controls col-sm-4 text-center">
+        controls
+    </div>
+</div>
+
+<div class="overflow-auto">
+    <table class="calendar month mb-3">
+        
+    </table>
+</div>
